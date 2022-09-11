913520d8eeb7463b166f4426dda9eaa0a914ff0e
baldockb2514 <111828862+baldockb2514@users.noreply.github.com>
2022-09-12 09:52:09 +1200

    Add files via upload

---
+++ 02_input_checker.py
@@ -0,0 +1,24 @@
+# checks user choice is 'integer', 'text' or 'image'
+def user_choice():
+
+    valid = False 
+    while not valid:
+
+        # sk user for choice and change response to lowercase
+        response = "File type (interger / text / image): ".lower()
+
+        # If they choose "t" or "text", return "text"
+        if response == "text" or response == "t":
+            return response
+        
+        else:
+            # if response is not valid, output an error
+            print(" PLease choose a vslif file type!")
+            print()
+
+
+# Main routine goes here
+data_type = user_choice()
+print("You chose", data_type)
+
+print()
